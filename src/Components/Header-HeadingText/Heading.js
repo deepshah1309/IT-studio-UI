435b0598c10e6566920922b8125f57aed510eeb9
deepshah1309 <deepshah1309@gmail.com>
2021-06-10 22:42:04 +0530

    Newchanges

--- src/Components/Header-HeadingText/Heading.js
+++ src/Components/Header-HeadingText/Heading.js
@@ -2,6 +2,8 @@ import "./Heading.css";
 import Typewriter from 'typewriter-effect';
 import Search from "../SearchBox/Search";
 import RightImg from "../../Image-Assets/people.jpg";
+import Circle from "../Circle/Circle";
+import SmallCircle from "../SmallCircle/SmallCircle";
 const Heading=()=>{
     return (
         <div className="Heading">
@@ -9,18 +11,20 @@ const Heading=()=>{
                         <div className="content">
                         <div className="UpperText">
                         
-                        The Best Service
+                        --The Best Service
+                        <div style={{position:"absolute",zIndex:"-3",top:"11rem",left:"5rem"}}><Circle/></div>
                         </div>
                         <div className="typingtext">
                         <Typewriter
                           
                           options={{
-                              strings: ["perfect place to have Optimized IT products"],
+                              strings: ["We give the best service for you"],
                               autoStart: true,
                               loop: true,
                           }}
                       />
-
+                      
+                        <div style={{position:"absolute",zIndex:"-3",top:"55%",left:"40%"}}><SmallCircle/></div>
                         </div>
                         <div className="text1">growth and success are never mere by chance.</div>
                         <div className="text2">its the result of the forces working together.</div>
--- src/Components/Navigation-Header/Navigation.js
+++ src/Components/Navigation-Header/Navigation.js
@@ -1,13 +1,12 @@
 import "./Navigation.css";
 import Logo from "../Logo/Logo";
 import Menu from "../Navigation-menu/Menu";
+
 const Navigation=()=>{
     return (
         <div className="Navigation">
                 <div className="part1"><Logo/></div>
-                <div className="part2">
-                   <Menu/>
-                </div>
+                <div className="part2"><Menu/></div>
         </div>
     )
 }
