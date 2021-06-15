8044497d11c92cf2d92b07457cea416ebaa9e388
deepshah1309 <deepshah1309@gmail.com>
2021-06-15 18:31:38 +0530

    updates

--- src/Components/WeBuild/WeBuild/WeBuild.js
+++ src/Components/WeBuild/WeBuild/WeBuild.js
@@ -3,7 +3,7 @@ import brand from "../../../Assets/weBuilt_assests/brand.png";
 import design from "../../../Assets/weBuilt_assests/design.png";
 import experience from "../../../Assets/weBuilt_assests/experience.png";
 import software from "../../../Assets/weBuilt_assests/software.png";
-import WebuildCard from "../WeBuildCard/WebuildCard";
+import WebuildCard from "../WeBuildCard/WeBuildCard";
 import "./WeBuild.css";
 const WeBuildData = [
   {
