37eba6af3eb8f075921c37f228897b4ecdbdbcc8
Md. Rishad Khan <rishadkhan190@gmail.com>
2021-06-16 20:49:13 +0600

    add hover part

--- src/Components/OurService/OurServiceCard/OurServiceCard.js
+++ src/Components/OurService/OurServiceCard/OurServiceCard.js
@@ -5,8 +5,17 @@ const OurServiceCard = (props) => {
   const { tittle, icon, classBox } = props.service;
   return (
     <div className={classBox}>
-      <h2>{tittle}</h2>
-      <img src={icon} alt="" />
+      <div className="our_service_content_box">
+        <h2>{tittle}</h2>
+        <img src={icon} alt={tittle} />
+      </div>
+      <div className="card_box_section_overlay">
+        <p>
+          No Successful Website Build Is An Accident. From The Technology,
+          Assembling Thr Right Team, Every Aspect Of The Effort Requires
+          Planing, Communication, And Experience.
+        </p>
+      </div>
     </div>
   );
 };
