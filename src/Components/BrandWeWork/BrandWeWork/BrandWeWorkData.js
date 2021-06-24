66e1d6c45df7c7364ea58664f49bdb204a086aa2
Md. Rishad Khan <rishadkhan190@gmail.com>
2021-06-24 17:23:43 +0600

    add brand

---
+++ src/Components/BrandWeWork/BrandWeWork/BrandWeWorkData.js
@@ -0,0 +1,29 @@
+import company from "../../../Assets/brand_we_build_assets/Polygon 109.png";
+import company1 from "../../../Assets/brand_we_build_assets/Polygon 105.png";
+import company2 from "../../../Assets/brand_we_build_assets/Polygon 106.png";
+import company3 from "../../../Assets/brand_we_build_assets/Polygon 107.png";
+
+const brandWeBuild = [
+  {
+    id: 1,
+    brand: company,
+    name: "",
+  },
+  {
+    id: 2,
+    brand: company1,
+    name: "",
+  },
+  {
+    id: 3,
+    brand: company2,
+    name: "",
+  },
+  {
+    id: 4,
+    brand: company3,
+    name: "",
+  },
+];
+
+export default brandWeBuild;
--- src/Components/BrandWeWork/BrandWeWorkCard/BrandWeWorkCard.js
+++ src/Components/BrandWeWork/BrandWeWorkCard/BrandWeWorkCard.js
@@ -1,7 +1,31 @@
 import React from "react";
+import "./BrandWeWorkCard.css";
+import Hexagon from "react-hexagon";
+import shadow from "../../../Assets/brand_we_build_assets/Polygon 1192.png";
+import shadow2 from "../../../Assets/brand_we_build_assets/Polygon 1194.png";
 
-const BrandWeWorkCard = () => {
-  return <div></div>;
+const BrandWeWorkCard = (props) => {
+  const { brand } = props.logo;
+  return (
+    <>
+      <div className="box1_hexagon">
+        <div>
+          <Hexagon flatTop style={{ stroke: "" }} backgroundImage={brand} />
+        </div>
+        <div>
+          <img src={shadow} alt="" />
+        </div>
+      </div>
+      <div className="box2_hexagon">
+        <div>
+          <Hexagon flatTop style={{ stroke: "" }} backgroundImage={brand} />
+        </div>
+        <div>
+          <img src={shadow2} alt="" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BrandWeWorkCard;
